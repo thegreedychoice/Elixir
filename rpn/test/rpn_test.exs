e1d83b64966e2a13471e40479240fc4a1ead4dc5
Shubham Shukla <shubham.shukla77@hotmail.com>
2017-09-12 21:19:25 -0400

    Created code to build a reverse polish calculator

---
+++ rpn/test/rpn_test.exs
@@ -0,0 +1,61 @@
+defmodule RpnTest do
+  use ExUnit.Case
+  doctest Rpn
+
+  test "greets the world" do
+    assert Rpn.hello() == :world
+  end
+
+  test "starts with an empty stack" do
+  	{:ok, pid} = Rpn.start
+  	assert Rpn.peek(pid) == []
+  end
+  test "pushing onto the stack" do
+  	{:ok, pid} = Rpn.start
+  	assert :ok == Rpn.push(pid, 5)
+  	assert Rpn.peek(pid) == [5]
+  	assert :ok == Rpn.push(pid, 1)
+  	assert Rpn.peek(pid) == [1,5]
+  end
+  test "add" do
+  	{:ok, pid} = Rpn.start
+  	assert :ok == Rpn.push(pid, 5)
+  	assert :ok == Rpn.push(pid, 1)
+  	assert :ok == Rpn.push(pid, :+)
+  	assert Rpn.peek(pid) == [6]
+  end
+  test "subtract" do
+  	{:ok, pid} = Rpn.start
+  	assert :ok == Rpn.push(pid, 5)
+  	assert :ok == Rpn.push(pid, 1)
+  	assert :ok == Rpn.push(pid, :-)
+  	assert Rpn.peek(pid) == [4]
+  end
+  test "multiply" do
+  	{:ok, pid} = Rpn.start
+  	assert :ok == Rpn.push(pid, 5)
+  	assert :ok == Rpn.push(pid, 2)
+  	assert :ok == Rpn.push(pid, :*)
+  	assert Rpn.peek(pid) == [10]
+  end
+  test "divide-" do
+  	{:ok, pid} = Rpn.start
+  	assert :ok == Rpn.push(pid, 5)
+  	assert :ok == Rpn.push(pid, 2)
+  	assert :ok == Rpn.push(pid, :/)
+  	assert Rpn.peek(pid) == [2.0]
+  end
+  test "wikipedia example" do
+    {:ok, pid} = Rpn.start
+    assert :ok == Rpn.push(pid, 5)
+    assert :ok == Rpn.push(pid, 1)
+    assert :ok == Rpn.push(pid, 2)
+    assert :ok == Rpn.push(pid, :+)
+    assert :ok == Rpn.push(pid, 4)
+    assert :ok == Rpn.push(pid, :x)
+    assert :ok == Rpn.push(pid, :+)
+    assert :ok == Rpn.push(pid, 3)
+    assert :ok == Rpn.push(pid, :-)
+    assert Rpn.peek(pid) == [14]
+  end
+end
